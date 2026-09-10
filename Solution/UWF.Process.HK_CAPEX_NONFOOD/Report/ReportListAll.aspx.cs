ec327c7102174ec0625a8b80b4a8e735a5b8684c
yang.han <yang.han.ext@sodexo.com>
2026-09-10 13:14:53 +0800

    11

--- Solution/UWF.Process.HK_CAPEX_NONFOOD/Report/ReportListAll.aspx.cs
+++ Solution/UWF.Process.HK_CAPEX_NONFOOD/Report/ReportListAll.aspx.cs
@@ -78,8 +78,8 @@ FROM (
         [APPROVE], [APPLICANTTEL], [COMPANY], [COSTCENTER], 
         '' AS [APPREMARK], [AMOUNT], [SUPPLIERCODE], [SUPPLIERNAME]
     FROM PROC_HK_CAPEX_NONFOOD
-    WHERE SITECODE = @SiteCode 
-      AND APPLICANTCODE = @ApplicantCode 
+    WHERE SITECODE = '{0} '
+      AND APPLICANTCODE ='{1}'
      -- AND REQUESTDATE > DATEADD(DAY, -60, GETDATE())
     
     UNION ALL
@@ -97,8 +97,8 @@ FROM (
         [APPROVE], [APPLICANTTEL], [COMPANY], [COSTCENTER], 
         '' AS [APPREMARK], [AMOUNT], [SUPPLIERCODE], [SUPPLIERNAME]
     FROM PROC_HK_CAPEX_SERVICE
-    WHERE SITECODE = @SiteCode 
-      AND APPLICANTCODE = @ApplicantCode 
+    WHERE SITECODE = '{0} '
+      AND APPLICANTCODE ='{1}'
      -- AND REQUESTDATE > DATEADD(DAY, -60, GETDATE())
 ) AS AAA
 -- 2. 使用 CROSS APPLY 高效获取每个表单的最新审批记录，彻底替代 ROW_NUMBER()
@@ -112,8 +112,7 @@ CROSS APPLY (
     ORDER BY ID DESC
 ) AS WF
 WHERE AAA.INCIDENT <> -1  -- 3. 保持类型一致，避免隐式转换导致索引失效
-ORDER BY AAA.REQUESTDATE DESC", pcCode, empNo
-);
+ORDER BY AAA.REQUESTDATE DESC", pcCode, empNo);
             //Ultimus.UWF.Form.WebControls.Repeater cptLog = Page.FindControl("OR_CPR_LOG") as Ultimus.UWF.Form.WebControls.Repeater;
             //cptLog.Source = string.Format();
         }
